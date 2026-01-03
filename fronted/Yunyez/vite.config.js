470f4314e866e47390cdd2e7277ec134ff52c08d
ITMA <ITMA@example.com>
2026-01-03 19:50:27 +0800

    合并mian

--- fronted/Yunyez/vite.config.js
+++ fronted/Yunyez/vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { viteMockServe } from 'vite-plugin-mock' // 导入Mock插件
 
 // https://vite.dev/config/
 export default defineConfig({
